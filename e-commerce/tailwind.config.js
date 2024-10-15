1d1022f6dbd80e4602e4f8b59cb34cd9e57ad9e5
Ömer Bayram <emaryabo@gmail.com>
2024-10-15 17:29:32 +0300

    header and footer created

--- e-commerce/tailwind.config.js
+++ e-commerce/tailwind.config.js
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      "2xl": { max: "1535px" },
+
+      xl: { max: "1279px" },
+
+      lg: { max: "1023px" },
+
+      sm: { max: "770px" },
+
+    },
     extend: {},
   },
   plugins: [],
-}
+};
+/*screens: {
+  tablet: { max: "767px" },
+
+  laptop: "1024px",
+  
+  desktop: "1280px",
+
+},*/
