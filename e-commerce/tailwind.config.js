76aabd64b7640e8e3a9492da3afa76a1a7ef3bb3
Ömer Bayram <emaryabo@gmail.com>
2024-10-21 09:23:40 +0300

    Revert "header and footer created"
    This reverts commit 1d1022f6dbd80e4602e4f8b59cb34cd9e57ad9e5.

--- e-commerce/tailwind.config.js
+++ e-commerce/tailwind.config.js
@@ -1,26 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    screens: {
-      "2xl": { max: "1535px" },
-
-      xl: { max: "1279px" },
-
-      lg: { max: "1023px" },
-
-      sm: { max: "770px" },
-
-    },
     extend: {},
   },
   plugins: [],
-};
-/*screens: {
-  tablet: { max: "767px" },
-
-  laptop: "1024px",
-  
-  desktop: "1280px",
-
-},*/
+}
