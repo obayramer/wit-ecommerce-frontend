81947d29aa27cb7b8f35a2fcddde7bb6dcf99047
Ömer Bayram <emaryabo@gmail.com>
2024-10-28 16:50:37 +0300

    t8 singup user form page completed

--- e-commerce/src/pages/SignUp.jsx
+++ e-commerce/src/pages/SignUp.jsx
@@ -1,5 +1,447 @@
-function SignUp() {
-    return <div className="SignUp">//todo</div>;
-  }
-  
-  export default SignUp;
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import "react-toastify/dist/ReactToastify.css";
+import axiosInstance from "../api/axiosInstance";
+import Spinner from "../components/Spinner";
+import { ToastContainer, toast } from "react-toastify";
+import { useHistory } from "react-router-dom";
+
+function SignUp({ data }) {
+  const {
+    header,
+    name,
+    email,
+    password,
+    passmatch,
+    role,
+    store,
+    button,
+    submission,
+  } = data.signup;
+  const [isLoading, setLoading] = useState(false);
+  const [isSubmitting, setSubmitting] = useState(false);
+  const [isStore, setStore] = useState(false);
+  const [roles, setRoles] = useState();
+  const history = useHistory();
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      role_id: "",
+      storename: "",
+      storephone: "",
+      storetaxid: "",
+      storeiban: "",
+    },
+    mode: "all",
+  });
+  const onSubmit = (formData) => {
+    setSubmitting(true);
+    axiosInstance
+      .post("/signup", formData)
+      .then((response) => {
+        setSubmitting(false);
+        window.confirm(`${response.message}\n${submission.success}`) &&
+          history.goBack();
+      })
+      .catch((error) => {
+        setSubmitting(false);
+        toast.error(`${submission.fail}`, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
+      });
+  };
+  useEffect(() => {
+    axiosInstance
+      .get("/roles")
+      .then((response) => {
+        setRoles(response.data);
+        setLoading(true);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  return (
+    <div className="SignUp bg-info min-h-screen py-10 px-80 sm:p-10">
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+      {isLoading ? (
+        <div>
+          <div className="p-12 font-bold flex flex-col gap-4 items-center text-center sm:text-center">
+            <h2 className="text-base text-accent sm:text-sm">
+              {header.subtitle}
+            </h2>
+            <h1 className="text-6xl leading-[5rem] sm:text-4xl">
+              {header.title}
+            </h1>
+            <p className="font-normal text-xl text-accent sm:text-base">
+              {header.description}
+            </p>
+          </div>
+
+          <form
+            className="flex flex-col gap-10"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <div className="form-group">
+              <label className="form-label" htmlFor="name">
+                {name.label}
+              </label>
+              <input
+                id="name"
+                className="form-input"
+                placeholder={name.placeholder}
+                {...register("name", {
+                  required: `${name.errorMsg.required}`,
+                  minLength: {
+                    value: 3,
+                    message: `${name.errorMsg.length}`,
+                  },
+                })}
+              />
+              {errors.name ? (
+                <p className="form-footnote text-red-600">
+                  {errors.name.message}
+                </p>
+              ) : (
+                <p className="form-footnote">{name.footnote}</p>
+              )}
+            </div>
+            <div className="form-group">
+              <label className="form-label" htmlFor="email">
+                {email.label}
+              </label>
+              <input
+                id="email"
+                className="form-input"
+                placeholder={email.placeholder}
+                type="email"
+                {...register("email", {
+                  required: `${email.errorMsg.required}`,
+                  validate: {
+                    matchPattern: (v) =>
+                      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
+                      "Email address must be a valid address",
+                  },
+                })}
+              />
+              {errors.email ? (
+                <p className="form-footnote text-red-600">
+                  {errors.email.message}
+                </p>
+              ) : (
+                <p className="form-footnote">{email.footnote}</p>
+              )}
+            </div>
+            <div className="form-group">
+              <label className="form-label" htmlFor="password">
+                {password.label}
+              </label>
+              <input
+                id="password"
+                className="form-input"
+                placeholder={password.placeholder}
+                type="password"
+                {...register("password", {
+                  required: `${password.errorMsg.required}`,
+                  minLength: {
+                    value: 8,
+                    message: `${password.errorMsg.length}`,
+                  },
+
+                  validate: {
+                    upperCase: (v) => {
+                      let isUpperCase = false;
+                      for (let char of v) {
+                        if (char >= "A" && char <= "Z") {
+                          isUpperCase = true;
+                          break;
+                        }
+                      }
+
+                      return isUpperCase
+                        ? isUpperCase
+                        : `${password.errorMsg.upper}`;
+                    },
+                    lowerCase: (v) => {
+                      let isLowerCase = false;
+                      for (let char of v) {
+                        if (char >= "a" && char <= "z") {
+                          isLowerCase = true;
+                          break;
+                        }
+                      }
+                      return isLowerCase
+                        ? isLowerCase
+                        : `${password.errorMsg.lower}`;
+                    },
+                    specialCase: (v) => {
+                      let isSpecialCase = false;
+                      const specialChars = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+                      for (let char of v) {
+                        if (specialChars.includes(char)) {
+                          isSpecialCase = true;
+                          break;
+                        }
+                      }
+                      return isSpecialCase
+                        ? isSpecialCase
+                        : `${password.errorMsg.special}`;
+                    },
+                  },
+                })}
+              />
+              {errors.password ? (
+                <p className="form-footnote text-red-600">
+                  {errors.password.message}
+                </p>
+              ) : (
+                <p className="form-footnote">{password.footnote}</p>
+              )}{" "}
+            </div>
+            <div className="form-group">
+              <label className="form-label" htmlFor="passmatch">
+                {passmatch.label}
+              </label>
+              <input
+                id="passmatch"
+                className="form-input"
+                placeholder={passmatch.placeholder}
+                type="password"
+                {...register("passmatch", {
+                  required: `${passmatch.errorMsg.required}`,
+                  validate: {
+                    matchesPass: (v) => {
+                      if (watch("password") !== v) {
+                        return `${passmatch.errorMsg.match}`;
+                      }
+                    },
+                  },
+                })}
+              />
+              {errors.passmatch ? (
+                <p className="form-footnote text-red-600">
+                  {errors.passmatch.message}
+                </p>
+              ) : (
+                <p className="form-footnote">{passmatch.footnote}</p>
+              )}
+            </div>
+            <div className="form-group">
+              <label className="form-label" htmlFor="role">
+                {role.label}
+              </label>
+              <select
+                id="role"
+                className="form-input"
+                defaultValue={roles[2].id}
+                {...register("role_id", {
+                  onChange: (e) => {
+                    if (e.target.value === roles[1].id) setStore(true);
+                    else setStore(false);
+                  },
+                })}
+              >
+                <option value={roles[2].id}>{role.customer}</option>
+                <option value={roles[0].id}>{role.admin}</option>
+                <option value={roles[1].id}>{role.store}</option>
+              </select>
+              <p className="form-footnote">{role.footnote}</p>
+            </div>
+            {isStore && (
+              <div className="flex flex-col gap-4 px-8">
+                <div className="form-group">
+                  <label className="form-label" htmlFor="storename">
+                    {store.name.label}
+                  </label>
+                  <input
+                    id="storename"
+                    className="form-input"
+                    placeholder={store.name.placeholder}
+                    {...register("storename", {
+                      required: `${store.name.errorMsg.required}`,
+                      minLength: {
+                        value: 3,
+                        message: `${store.name.errorMsg.length}`,
+                      },
+                    })}
+                  />
+                  {errors.storename ? (
+                    <p className="form-footnote text-red-600">
+                      {errors.storename.message}
+                    </p>
+                  ) : (
+                    <p className="form-footnote">{store.name.footnote}</p>
+                  )}
+                </div>
+                <div className="form-group">
+                  <label className="form-label" htmlFor="storephone">
+                    {store.phone.label}
+                  </label>
+                  <input
+                    id="storephone"
+                    className="form-input"
+                    placeholder={store.phone.placeholder}
+                    {...register("storephone", {
+                      required: `${store.phone.errorMsg.required}`,
+                      pattern: {
+                        value: /^\+90\d{10}$/,
+                        message: `${store.phone.errorMsg.format}`,
+                      },
+                    })}
+                  />
+                  {errors.storephone ? (
+                    <p className="form-footnote text-red-600">
+                      {errors.storephone.message}
+                    </p>
+                  ) : (
+                    <p className="form-footnote">{store.phone.footnote}</p>
+                  )}
+                </div>
+
+                <div className="form-group">
+                  <label className="form-label" htmlFor="storetaxid">
+                    {store.taxid.label}
+                  </label>
+                  <input
+                    id="storetaxid"
+                    className="form-input"
+                    placeholder={store.taxid.placeholder}
+                    {...register("storetaxid", {
+                      onChange: (event) => {
+                        let { value, selectionStart } = event.target;
+                        const numericValue = value.replace(/[^\d]/g, "");
+                        const totalDigits = numericValue.length;
+
+                        // Restrict the total number of digits to 20
+                        const truncatedValue = numericValue.slice(0, 10);
+
+                        // Calculate the cursor position
+                        let cursorPosition;
+
+                        if (selectionStart <= 1) {
+                          // Ensure cursor is after 'T'
+                          cursorPosition = 2;
+                        } else if (selectionStart <= 5) {
+                          // Ensure cursor is after 4 digits
+                          cursorPosition = selectionStart;
+                        } else {
+                          // Ensure cursor is after 4 digits
+                          cursorPosition = selectionStart + 1;
+                        }
+
+                        // Create the formatted value
+                        const formattedValue = `T${truncatedValue.slice(
+                          0,
+                          4
+                        )}V${truncatedValue.slice(4)}`;
+
+                        // Update the value in the input and set cursor position
+                        event.target.value = formattedValue;
+                        event.target.setSelectionRange(
+                          cursorPosition,
+                          cursorPosition
+                        );
+                      },
+                      required: `${store.taxid.errorMsg.required}`,
+                      minLength: {
+                        value: 12,
+                        message: `${store.taxid.errorMsg.length}`,
+                      },
+                    })}
+                  />
+                  {errors.storetaxid ? (
+                    <p className="form-footnote text-red-600">
+                      {errors.storetaxid.message}
+                    </p>
+                  ) : (
+                    <p className="form-footnote">{store.taxid.footnote}</p>
+                  )}
+                </div>
+
+                <div className="form-group">
+                  <label className="form-label" htmlFor="storeiban">
+                    {store.bank.label}
+                  </label>
+                  <input
+                    className="form-input"
+                    placeholder={store.bank.placeholder}
+                    {...register("storeiban", {
+                      required: `${store.bank.errorMsg.required}`,
+                      minLength: {
+                        value: 26,
+                        message: `${store.bank.errorMsg.minlength}`,
+                      },
+                      maxLength: {
+                        value: 26,
+                        message: `${store.bank.errorMsg.maxlength}`,
+                      },
+                      onChange: (event) => {
+                        let inputValue = event.target.value;
+
+                        if (!inputValue.startsWith("TR")) {
+                          inputValue = "TR";
+                        }
+                        const numericValue = inputValue.replace(/[^\d]/g, "");
+
+                        const formattedValue = `TR${numericValue}`;
+
+                        event.target.value = formattedValue;
+                      },
+                    })}
+                  />
+                  {errors.storeiban ? (
+                    <p className="form-footnote text-red-600">
+                      {errors.storeiban.message}
+                    </p>
+                  ) : (
+                    <p className="form-footnote">{store.bank.footnote}</p>
+                  )}
+                </div>
+              </div>
+            )}
+            <button
+              type="submit"
+              disabled={!isValid || isSubmitting}
+              className={
+                !isSubmitting && isValid
+                  ? "blue-button mx-auto flex gap-4 items-center"
+                  : "blue-button mx-auto flex gap-4 items-center bg-secondary-focus"
+              }
+            >
+              <span>{button}</span>
+              <span>{isSubmitting && <Spinner className="text-white" />}</span>
+            </button>
+          </form>
+        </div>
+      ) : (
+        <Spinner page={true} />
+      )}
+    </div>
+  );
+}
+export default SignUp;
--- e-commerce/src/reducers/dataReducer.jsx
+++ e-commerce/src/reducers/dataReducer.jsx
@@ -739,6 +739,107 @@ export default function dataReducer() {
             },
           ],
         },
+        signup: {
+          header: {
+            subtitle: "Join Our Shopping Community",
+            title: "Create Your Account",
+            description:
+              "Sign up and become a member to unlock a world of shopping delights. Let's get started on your shopping journey!",
+          },
+          name: {
+            label: "Name:",
+            placeholder: "Name *",
+            footnote: "Please type your name and surname.",
+            errorMsg: {
+              required: "Name field cannot be empty.",
+              length: "Name must be at least 3 characters.",
+            },
+          },
+          email: {
+            label: "Email:",
+            placeholder: "Email *",
+            footnote: "We'll never share your email with anyone else.",
+            errorMsg: {
+              required: "Email field cannot be empty.",
+            },
+          },
+          password: {
+            label: "Password:",
+            placeholder: "Password *",
+            footnote: "Set a password.",
+            errorMsg: {
+              required: "Password field cannot be empty.",
+              length: "Name must be at least 8 characters.",
+              upper: "Password must include at least one upper case character.",
+              lower: "Password must include at least one lower case character.",
+              special:
+                "Password must include at least one special character: !#$%&'()*+,-./:;<=>?@[]^_`{|}~",
+            },
+          },
+          passmatch: {
+            label: "Confirm Password:",
+            placeholder: "Re-enter your password *",
+            footnote: "Please re-enter your password.",
+            errorMsg: {
+              required: "You must re-enter your password.",
+              match: "Passwords must match.",
+            },
+          },
+          role: {
+            label: "Role:",
+            admin: "Admin",
+            store: "Store",
+            customer: "Customer",
+            footnote: "Choose a role from the list.",
+          },
+          store: {
+            name: {
+              label: "Store Name:",
+              placeholder: "Store Name *",
+              footnote: "Enter the store name.",
+              errorMsg: {
+                required: "Store name field cannot be empty.",
+                length: "Store name must be at least 3 characters.",
+              },
+            },
+            phone: {
+              label: "Store Phone:",
+              placeholder: "+90XXXXXXXXXX *",
+              footnote: "Enter a valid Turkish phone number.",
+              errorMsg: {
+                required: "Phone field cannot be empty.",
+                format: "Phone number must be in the format +90 followed by 10 digits.",
+              },
+            },
+            taxid: {
+              label: "Store Tax ID:",
+              placeholder: "TXXXXVXXXXXX *",
+              footnote: "Enter the store tax ID.",
+              errorMsg: {
+                required: "Tax ID field cannot be empty.",
+                length: "Tax ID must be 12 characters (including letters).",
+              },
+            },
+            bank: {
+              label: "Store Bank Account:",
+              placeholder: "Store Bank Account *",
+              footnote: "Enter the IBAN adress.",
+              errorMsg: {
+                required: "Bank account field cannot be empty.",
+                minlength:
+                  "IBAN cannot be less than 26 characters, including country code.",
+                maxlength:
+                  "IBAN cannot be more than 26 characters, including country code.",
+              },
+            },
+          },
+          button: "Submit",
+          submission: {
+            success: "You need to click link in email to activate your account!",
+            fail: "Something went wrong. Please try again.",
+          },
+        },
+  
   
         team: {
           section: {
